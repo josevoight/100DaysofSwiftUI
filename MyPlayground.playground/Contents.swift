5fefa199b7e00a5f5e2a1e41f970d9f257ef2c21
voight <josevoight@gmail.com>
2020-07-14 23:34:40 -0700

    Update Contents.swift
    added examples of var, strings, doubles, booleans, constants

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,46 @@ import UIKit
 
 var str = "Hello, playground"
 str = "Good bye"
+
+
+//: Strings and integers
+var age = 38
+var population = 8_000_000 //: using underscore as thousands separators
+
+//: Multi-line strings
+var  str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multilple \
+lines
+"""
+
+//:  Doubles and boolenas
+//: doubles = fraction
+//: booleans = true or false
+var pi = 3.141
+var awesome = true
+
+
+//: String interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+//: Constants
+//: use "let" keyboard and never change it values
+let taylor = "swift"
+
+//: Type annotations
+
+let str4 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks : Bool = true 
+
