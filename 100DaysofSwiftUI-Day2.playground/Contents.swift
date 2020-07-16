8fd2b9193b0818ffbf2e746b0021563b064d4045
voight <josevoight@gmail.com>
2020-07-15 23:17:45 -0700

    added to content.swift
    more info about enum and examples

--- 100DaysofSwiftUI-Day2.playground/Contents.swift
+++ 100DaysofSwiftUI-Day2.playground/Contents.swift
@@ -78,4 +78,22 @@ enum Result {
 
 let result5 = Result.failure
 
-// Enum associated value 
+// Enum associated value
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "football")
+
+// Enum raw values
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet(rawValue: 2)
