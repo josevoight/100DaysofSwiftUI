3d80f65cda6709eb4d7d9ae61d6841d0efee6b10
voight <josevoight@gmail.com>
2020-07-17 21:58:36 -0700

    Contents.swift
    Day 4: added an example of for loops and while loops

---
+++ 100DaysofSwiftUI-Day4.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+//: 100 Days of SwiftUI Day 4
+// For loops
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Taylor Swift", "Red", "1989", "Reputation", "Lover"]
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+// While loops
+var number2 = 1
+
+while number2 <= 20 {
+print(number2)
+number2 += 1
+    
+}
+
+print("Ready or not, here I come")
+
+
