c85e79b6d3b79c35989214e394f3220bb1aee86f
voight <josevoight@gmail.com>
2020-07-19 23:49:57 -0700

    Update Contents.swift
    more ways to use closure

--- 100DaysofSwiftUI-Day6.playground/Contents.swift
+++ 100DaysofSwiftUI-Day6.playground/Contents.swift
@@ -13,3 +13,31 @@ let driving1 = { (place: String) in
 }
  
 driving1("London")
+
+
+// Returning values from a closure
+let driving1 = { (place1: String) in
+    print("I'm going to \(place1) in my car")
+    
+}
+
+let drivingWithReturn = { (place2: String) -> String in
+    return "I'm going to \(place2) in my car"
+}
+
+let message = drivingWithReturn("London")
+print(message)
+
+// closures as parameters
+let driving2 = {
+    print("I'm driving in my car")
+}
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving)
+
