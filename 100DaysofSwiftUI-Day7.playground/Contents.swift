8936b97acabf23f6b64d265eb2c9f6cc494ad1e8
voight <josevoight@gmail.com>
2020-07-20 23:55:46 -0700

    Update Contents.swift
    Day 7: added shorthand parameter names

--- 100DaysofSwiftUI-Day7.playground/Contents.swift
+++ 100DaysofSwiftUI-Day7.playground/Contents.swift
@@ -23,3 +23,15 @@ func travel1(action: (String) -> String) {
 travel1 { (place: String) -> String in
     return "I'm going to \(place) in my car"
 }
+
+//Shorthand parameter names
+func travel2(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel2 { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
