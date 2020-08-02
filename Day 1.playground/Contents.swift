77cd41ddea4550f80b408334a59370b4740e3f21
voight <josevoight@gmail.com>
2020-08-01 23:55:37 -0700

    Day 1
    review of variables, integers, and multiple lines strings

--- Day 1.playground/Contents.swift
+++ Day 1.playground/Contents.swift
@@ -4,3 +4,20 @@ import UIKit
 var str = "Hello, playground"
 str = "I am Taylor"
 
+// Storing age integers
+var age = 30
+var population = 8_000_000
+
+// Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+
+This goes \
+over multiple \
+lines
+"""
