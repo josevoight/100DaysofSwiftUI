84505e88a1c846f071a204dd3f6e5877993d5ec1
voight <josevoight@gmail.com>
2020-08-03 23:34:27 -0700

    Day 2 continue
    Tuples: a way to access data

--- Day 2.playground/Contents.swift
+++ Day 2.playground/Contents.swift
@@ -12,3 +12,13 @@ beatles[1]
 // Sets
 let colors =  Set(["red", "green", "blue"])
 let colors2 = Set (["red", "green", "blue", "red", "blue"])
+
+///Tuples
+var name = (first: "Taylor", last: "Swift")
+//accesing tuples starting from 0
+name.0
+//other way
+name.first
+
+
+/// Arrays vs Sets vs Tuple
