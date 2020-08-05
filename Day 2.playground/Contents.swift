896217412e25ed7830ba93479e45a81b7c728802
voight <josevoight@gmail.com>
2020-08-04 23:54:35 -0700

    Day 2 continue
    enum associated values

--- Day 2.playground/Contents.swift
+++ Day 2.playground/Contents.swift
@@ -87,3 +87,12 @@ enum Result {
 
 let result6 = Result.failure
 
+// enum associated values
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "football")
