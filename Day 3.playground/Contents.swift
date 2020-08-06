b4cff2437b7a9095d420595d5746fbd65ee8f0fc
voight <josevoight@gmail.com>
2020-08-05 23:56:23 -0700

    Day 3 continue
    arithmetic operators

--- Day 3.playground/Contents.swift
+++ Day 3.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
 var str = "Hello, this is Day 3 of learning Swift"
+// Arithmetic operators
+let firstScore = 12
+let secondScore = 4
+
+//
+let total = firstScore + secondScore
+let different = firstScore - secondScore
+
+//
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+//
+let remainder = 13 % secondScore
