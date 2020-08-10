cbccdead88219acb399a0fc193036afcb7418a4a
voight <josevoight@gmail.com>
2020-08-09 23:53:25 -0700

    Day 4
    for loops

---
+++ Day 4.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//for loops
+let count = 1...10
+
+// use for loop to print each item
+for number in count {
+    print("Number is \(number)")
+}
+
+// for loops
