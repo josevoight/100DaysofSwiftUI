4ee55778399605755d5f8aed77ca07906ae2457b
voight <josevoight@gmail.com>
2020-08-11 23:53:50 -0700

    Day 4 continue
    exiting multiple loops

--- Day 4.playground/Contents.swift
+++ Day 4.playground/Contents.swift
@@ -66,3 +66,32 @@ while countDown1 >= 0 {
     
     countDown1 -= 1
 }
+
+// exiting multiple loops
+for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+    }
+}
+
+//outerLoop:
+for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+    }
+}
+
+//
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
