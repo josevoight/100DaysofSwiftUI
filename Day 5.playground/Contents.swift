b56c2a120805090fda7d5fe4f681fd51c2369dd4
voight <josevoight@gmail.com>
2020-08-12 23:58:11 -0700

    Day 5
    this is a function

---
+++ Day 5.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//writing a functionn
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+// run the functon
+printHelp()
