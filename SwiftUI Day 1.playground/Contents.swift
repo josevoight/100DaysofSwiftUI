96edc44549ade0474777b218c663736f7f24ffb3
voight <josevoight@gmail.com>
2020-08-16 23:42:40 -0700

    SwiftUI Day 1
    strings, integers, and multi-line strings

--- SwiftUI Day 1.playground/Contents.swift
+++ SwiftUI Day 1.playground/Contents.swift
@@ -2,3 +2,14 @@ import UIKit
 
 var str = "Hello, playground"
 str = "Hello, it's Taylor Swift"
+
+// Strings and Integers
+var age = 38
+var population = 8_000_000
+
+//Multi-line strings
+var str1 = """
+This goes \
+over multiple \
+lines
+"""
